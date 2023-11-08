05708cec7c21d5b4b1e9046398cc12ca3c98ce23
Begimay11 <begimaysagynbaeva245@gmail.com>
2023-11-08 19:10:12 +0600

    Motion_project

---
+++ script.js
@@ -0,0 +1,14 @@
+// const img = document.querySelector(".avto_img");
+// const p = document.querySelector(".auto_text");
+
+// p.style.display = "none";
+
+// img.addEventListener("mouseover", () => {
+//   p.style.display = "block";
+//   p.style.color = 'white'
+//   p.style.transtion = ".5s";
+// });
+// img.addEventListener("mouseleave", () => {
+//   p.style.display = "none";
+//   p.style.transtion = ".5s";
+// });
